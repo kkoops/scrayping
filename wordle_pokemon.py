6923eec6d2300cc2904baa99a93ea980e1e65904
kkoops <kthr242468@icloud.com>
2022-01-31 20:28:12 +0900

    一応動く

--- wordle_pokemon.py
+++ wordle_pokemon.py
@@ -1,13 +1,38 @@
+def count_index(word,str_g):
+    count=0
+    for i in range(5):
+        if str_g[i]==word[i]:
+            count=count+1
+    return count
+def count_max_length(str_g):
+    count=0
+    for i in range(5):
+        if str_g[i] is not None:
+            count=count+1
+    return count
+def count_not_match(word,str_b):
+    count=0
+    for alp in str_b:
+        if alp in word:
+            count=count+1
+    return count==0
+def count_match(word,str_y):
+    count=0
+    for alp in str_y:
+        if alp in word:
+            count=count+1
+    return count==len(str_y)
 pokemon_list=[]
-black=[]
+black=["ト","セ","ル","ド","カ","ス"]
 yellow=[]
-green=[]
+green=[None,"ン",None,"ラ",None]
+maxlength=count_max_length(green)
 with open('pokemonlistfile.txt') as f:
     lines=f.read()
     for i in lines.split("\n"):
        pokemon_list.append(i)
 #print(pokemon_list)
 for i in pokemon_list:
-    if ('ン' in i and 'ル'in i and 'ラ' in i and 'ス'in i):
+    if len(i)==5  and count_index(i,green)==maxlength and count_not_match(i,black)==True and count_match(i,yellow)==True:
         print(i)
     
